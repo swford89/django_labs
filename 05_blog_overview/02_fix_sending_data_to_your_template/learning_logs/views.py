0fa8e00270752fc981d0b04d8b24d2505e5767c9
Scott Ford <swford@ualberta.ca>
2021-06-12 14:38:37 -0600

    Added context dictionary to topics function; formatted context dictionary for entries

--- 05_blog_overview/02_fix_sending_data_to_your_template/learning_logs/views.py
+++ 05_blog_overview/02_fix_sending_data_to_your_template/learning_logs/views.py
@@ -10,10 +10,12 @@ def index(request):
 def topics(request):
     """Displays a list of all available topics."""
     topics = Topic.objects.order_by('-date_added')
-    return render(request, 'learning_logs/topics.html')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
 
 def entry(request, pk):
     """Displays the full text of a learning entry."""
     entry = Entry.objects.get(pk=pk)
-    return render(request, 'learning_logs/entry.html', {'entry': entry})
+    context = {'entry': entry}
+    return render(request, 'learning_logs/entry.html', context)
     
