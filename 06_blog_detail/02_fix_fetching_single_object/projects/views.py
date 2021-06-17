6bff72a11282a519c01e510240109f46c6b0d753
Scott Ford <swford@ualberta.ca>
2021-06-17 17:37:03 -0600

    formatted context dictionary; added primary key parameter to detail function; formatted database call to access project with queried pk

--- 06_blog_detail/02_fix_fetching_single_object/projects/views.py
+++ 06_blog_detail/02_fix_fetching_single_object/projects/views.py
@@ -4,9 +4,13 @@ from .models import Project
 
 def index(request):
     projects = Project.objects.all()
-    return render(request, 'projects/index.html', {'projects': projects})
+    context = {'projects': projects}
+    return render(request, 'projects/index.html', context)
 
 
-def detail(request):
+def detail(request, pk):
     # TODO: how can I get a single object from the database again?
-    return render(request, 'projects/detail.html', {'project': project})
+    # DONE
+    project = Project.objects.get(pk=pk)
+    context = {'project': project}
+    return render(request, 'projects/detail.html', context)
