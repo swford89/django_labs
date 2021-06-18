3c91eb141ae6ae37e353c06bddb472d0a4666a72
Scott Ford <swford@ualberta.ca>
2021-06-18 13:16:51 -0600

    added path for slugs; kept primary key path

--- 06_blog_detail/03_fix_path_converting/projects/urls.py
+++ 06_blog_detail/03_fix_path_converting/projects/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'projects'
 
 urlpatterns = [
-    path('', views.index, name='projects'),
-    path('<int:pk>', views.detail, name='project_detail'),
+    path('', views.index, name='index'),
+    path('<int:pk>', views.detail_pk, name='detail_pk'),
+    path('<str:slug>', views.detail_slug, name='detail_slug')
 ]
