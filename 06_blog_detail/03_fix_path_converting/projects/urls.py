d0d96868a3856c03bcb73737b5f94629708bfa0b
Scott Ford <swford@ualberta.ca>
2021-06-21 17:03:42 -0600

    removed path for pk; kept path for a slug

--- 06_blog_detail/03_fix_path_converting/projects/urls.py
+++ 06_blog_detail/03_fix_path_converting/projects/urls.py
@@ -6,6 +6,5 @@ app_name = 'projects'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:pk>', views.detail_pk, name='detail_pk'),
-    path('<str:slug>', views.detail_slug, name='detail_slug')
+    path('<slug:slug>', views.detail_slug, name='detail_slug')
 ]
